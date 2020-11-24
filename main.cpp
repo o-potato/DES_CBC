ddcfa7561cff5bd22794ff0bdf0fda44b1b9c68b
o-potato <53241841+o-potato@users.noreply.github.com>
2020-11-24 20:10:14 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "des.h"
+using namespace std;
+
+int main()
+{
+	int mode;
+	cout << "请选择加密模式 or 解密模式：" << endl << "0 - 加密" << endl << "1 - 解密" << endl;
+	cin >> mode;
+
+	string file_path;
+	cout << "请输入密钥文件(.txt文件)路径：" << endl;
+	cin >> file_path;
+
+	//创建一个文件流对象,并打开存储密钥的.txt文件
+	ifstream keyfile(file_path);
+	if (!keyfile) {
+		cout << "Unable to open the file!" << endl;
+		exit(1);
+	}
+	//获取密钥
+	string key;
+	keyfile >> key;
+
+	cout << "请输入需加密/解密文件(.txt文件)的路径：" << endl;
+	cin >> file_path;
+
+	//打开存储需加密/解密的文件
+	ifstream datafile(file_path);
+	if (!datafile) {
+		cout << "Unable to open the file!" << endl;
+		exit(1);
+	}
+	//将文件读入到ostringstream字符串流对象buf中
+	ostringstream buf;
+	buf << datafile.rdbuf(); // 把文件流中的字符输入到字符串流中
+	//返回与流对象buf关联的字符串
+	string text = buf.str();
+	//cout << "data: ";
+	//cout << text << endl << endl;
+
+	//char c = '0';
+	//cout << text.length() << endl;
+	//cout << text << endl;
+	while (text.length() % 64 != 0)	//明文不足64位自动补0
+		text = text + '0';
+	//cout << text.length() << endl;
+	//cout << text << endl;
+
+	string cipher;
+	string plaintext;
+	ofstream result_file("result.txt");
+
+	//使用分组链接模式
+	string init_vector = key;	//设定分组链接的初始向量为密钥
+	if (mode == 0) {
+		cipher = CBC(text, key, init_vector, en);
+		result_file << cipher;
+	}
+	if (mode == 1) {
+		plaintext = CBC(text, key, init_vector, de);
+		result_file << plaintext;
+	}
+
+	cout << "Your result is saved in result.txt!" << endl;
+
+	keyfile.close();
+	datafile.close();
+	result_file.close();
+
+	system("pause");
+	return 0;
+}
+
+
+//string byte2bit(string byte)
+//{//字符串转比特串
+//	int length = byte.length();
+//	string bit(length * 8, 0);
+//	for (int i = 0; i < length; i++) {
+//		for (int j = 0; j < 8; j++) {
+//			bit[i * 8 + j] = (byte[i] >> (7 - j)) & 1;
+//		}
+//	}
+//	return bit;
+//}
+
+//string bit2byte(string bit)
+//{//比特串转字符串
+//	int length = bit.length() / 8;
+//	string byte(length, 0);
+//	for (int i = 0; i < length; i++)
+//	{
+//		byte[i] = 0;
+//		for (int j = 0; j < 8; j++)
+//			byte[i] = (byte[i] << 1) + bit[i * 8 + j];
+//	}
+//	return byte;
+//}
+
+//string hex2bit(string hex)
+//{//十六进制字符串转比特串
+//	int length = hex.length();
+//	string bit(length * 4, 0);
+//	for (int i = 0; i < length; i++)
+//	{
+//		hex[i] -= 48;
+//		if (hex[i] > 9)
+//			hex[i] -= 7;
+//		for (int j = 0; j < 4; j++)
+//			bit[i * 4 + j] = (hex[i] >> (3 - j)) & 1;
+//	}
+//	return bit;
+//}
+
+//string bit2hex(string bit)
+//{//比特串转十六进制字符串
+//	int length = bit.length() / 4;
+//	string hex(length, 0);
+//	for (int i = 0; i < length; i++)
+//	{
+//		hex[i] = 0;
+//		for (int j = 0; j < 4; j++)
+//			hex[i] = (hex[i] << 1) + bit[i * 4 + j];
+//		hex[i] += 48;
+//		if (hex[i] > 57)
+//			hex[i] += 7;
+//	}
+//	return hex;
+//}
+
+//void output(string s)
+//{//输出二进制字符串
+//	cout << s.length() << "\t";
+//	for (int i = 0; i < s.length(); i++)
+//	{
+//		if (s[i] == 1)
+//			cout << 1;
+//		else
+//			cout << 0;
+//	}
+//	cout << endl;
+//}
+
